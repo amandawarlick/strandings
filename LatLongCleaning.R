971f7db7ef4ad05826334957ec6ecf37f300d70c
amandawarlick <amandajwarlick@gmail.com>
2017-02-26 08:24:35 -0800

    Feb update.

--- LatLongCleaning.R
+++ LatLongCleaning.R
@@ -1,3 +1,5 @@
+options(java.parameters = "-Xmx80000m")
+library(xlsx)
 library(tidyr)
 library(ggmap)
 library(data.table)
@@ -14,6 +16,7 @@ library(magrittr)
 
 pinnipeds_data <- read.csv("pinnipeds_data.csv", header = TRUE, na.strings = "", stringsAsFactors = FALSE)
 
+#Pacific northwest
 all.data.orig <- read.csv("1989_2015_raw.csv", header = TRUE, na.strings = "", stringsAsFactors = FALSE) %>%
   filter(State != 'CA') %>%
   select(c(National.Database.Number, Field.Number, Genus, Common.Name, Affiliation, City...from.strandings.table, County, State, Locality.Detail,
@@ -79,4 +82,31 @@ Lat_Long_ordered <- all.data.orig %>%
   arrange(Latitude)
 
 write.csv(Lat_Long_ordered, file = "~/Documents/R/Strandings/Lat_Long_ordered.csv", row.names = F)
+
+###California
+
+#Read in
+all.data.orig.ca <- read.csv("CA_data_raw.csv", header = TRUE, na.strings = "", stringsAsFactors = FALSE)
+
+all.data.orig.ca <- all.data.orig.ca %>%
+  transform(Latitude = as.numeric(Latitude), Longitude = as.numeric(Longitude)) %>%
+  transform(Longitude = Longitude*(-1))
+
+mean_lat_long_ca <- all.data.orig.ca %>%
+  filter(Latitude > 31.5 & Latitude < 42) %>%
+  filter(Longitude > -125 & Longitude < -116) %>%
+  group_by(County) %>%
+  summarize(mean_lat = mean(Latitude, na.rm = T), mean_long = mean(Longitude, na.rm = T))
+
+Lat_Long_ordered_ca <- all.data.orig.ca %>%
+  merge(mean_lat_long_ca, by = "County") %>%
+  transform(Long_error = mean_long - Longitude,
+            Lat_error = mean_lat - Latitude) %>%
+  filter(Long_error < -1 | Long_error > 1 | Lat_error < -.5 | Lat_error > .5) %>%
+  select(Field.Number, National.Database.Number, Affiliation, Common.Name, Observation.Date,
+         State, County, City...from.strandings.table, Locality.Detail, Latitude, mean_lat,
+         Lat_error, Longitude, mean_long, Long_error) %>%
+  arrange(Latitude)
+
+write.csv(Lat_Long_ordered_ca, file = "~/Documents/R/Strandings/Lat_Long_ordered_ca.csv", row.names = F)
 
