5a1c7fd7f054c55ef9a36214d31db0cddd7a5824
Amanda Warlick <amandawarlick@Amandas-MacBook-Pro.local>
2020-07-23 12:58:35 -0700

    pushing to main

--- ENSO_Mapping/ENSO Data Cleaning.R
+++ ENSO_Mapping/ENSO Data Cleaning.R
@@ -1,44 +1,27 @@
 
 
-setwd("~/Documents/R/Strandings/ENSO_Mapping/Env Data")
+setwd("~/Documents/Research/Strandings/ENSO_Mapping/Env Data")
 
 ###Fish
 
-#Anchovy CPUE, natural log of catch+1, 44-46N
-Anch_CPUE <- read.csv("Anch_CPUE.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
-#Anchovy CPUE, natural log of catch+1, calCOFI lines?
-Anchovy_South <- read.csv("Anchovy_South.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
+MSquid <- read.csv("MSquid_N.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
+Herring <- read.csv("Herring_NCC.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
+Age1Sard <- read.csv("Age1Sard_NCC.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
+AGe1Anch <- read.csv("Age1Anch_NCC.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
 #Copepod biomass anomaly north, monthly, log(anomaly), 44N
 CBA_North <- read.csv("CBA_North.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
-#Copepod biomass anomaly south, monthly, log(anomaly), 44N
 CBA_South <- read.csv("CBA_South.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
-#Ratio of running mean of spawning escapements
-Chin_growth <- read.csv("Chin_growth.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
-#Chinook escapement anomalies, Columbia River
-Chinook_escape <- read.csv("Chinook_escape.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
-#Total coastwide coastal pelagics landings, thousand mt
-CPS_lands <- read.csv("CPS_lands.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
-#Herring CPUE nat log catch+1, 44N
-Herring_CPUE <- read.csv("Herring_CPUE.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
-#Total salmon landings, thousand mt, pacfin
-Salmon_lands <- read.csv("Salmon_lands.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
-#Sardine CPUE nat log catch+1, 44N
-Sardine_CPUE <- read.csv("Sardine_CPUE.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
-#Sardine CPUE nat log catch+1, CalCOFI line
-Sardine_South <- read.csv("Sardine_South.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
-#Total coastwide comm and rec fisheries landings thousand mt
-Tot_lands <- read.csv("Tot_lands.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE)
 
-fish <- Anch_CPUE %>% merge(Anchovy_South, by = 'Date', all = T) %>%
-  merge(CBA_North, by = 'Date', all = T) %>% merge(CBA_South, by = 'Date', all = T) %>%
-  merge(Chin_growth, by = 'Date', all = T) %>% merge(Chinook_escape, by = 'Date', all = T) %>% 
-  merge(CPS_lands, by = 'Date', all = T) %>% merge(Herring_CPUE, by = 'Date', all = T) %>% 
-  merge(Salmon_lands, by = 'Date', all = T) %>% merge(Sardine_South, by = 'Date', all = T) %>% 
-  merge(Sardine_CPUE, by = 'Date', all = T) %>% merge(Tot_lands, by = 'Date', all = T) %>%
+fish <- CBA_South %>% 
+  merge(CBA_North, by = 'Date', all = T) %>% 
+  merge(MSquid, by = 'Date', all = T) %>% 
+  merge(Herring, by = 'Date', all = T) %>%  
+  merge(AGe1Anch, by = 'Date', all = T) %>% 
+  merge(Age1Sard, by = 'Date', all = T) %>%
   transform(Date = as.Date(Date)) 
 fish$Year.of.Observation <- as.numeric(format(as.Date(fish$Date, format = "%Y-%M-%D"),"%Y"))
 fish$Mo <- as.numeric(format(as.Date(fish$Date, format = "%Y-%m-%D"),"%m"))
-fish <- fish %>% filter(Year.of.Observation > 1988) %>%
+fish <- fish %>% filter(Year.of.Observation > 2002) %>%
   transform(Month.of.Observation = ifelse(Mo == 1, 'JAN', 
                                           ifelse(Mo == 2, 'FEB',
                                                  ifelse(Mo == 3, 'MAR',
@@ -57,34 +40,58 @@ fish <- fish %>% filter(Year.of.Observation > 1988) %>%
 #http://oceanview.pfeg.noaa.gov/cciea-table/?opentab=1
 
 #Multivariate ENSO Index
-MEI <- read.csv("MEI.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
+MEI <- read.csv("MEI.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) %>%
+  transform(Date = sub('T.*', '', Date)) %>%
+  transform(Date = as.Date(Date, format = "%Y-%m-%d"))
+NOI <- read.csv("NOI.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) %>%
+  select(Date, NOI) %>%
+  transform(Date = sub('T.*', '', Date)) %>%
+  transform(Date = as.Date(Date, format = "%Y-%m-%d"))
 #North Pac Gyre Osc Index
-NPGO <- read.csv("NPGO.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
+NPGO <- read.csv("NPGO.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) %>%
+  transform(Date = sub('T.*', '', Date)) %>%
+  transform(Date = as.Date(Date, format = "%Y-%m-%d"))
 #Pacific Decadal Osc Index
-PDO <- read.csv("PDO.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
+PDO <- read.csv("PDO.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) %>%
+  transform(Date = sub('T.*', '', Date)) %>%
+  transform(Date = as.Date(Date, format = "%Y-%m-%d"))
 #SST degrees C, 39N
-SST_39 <- read.csv("SST_39.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
+SST_39 <- read.csv("SST_39.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) %>%
+  transform(Date = sub('T.*', '', Date)) %>%
+  transform(Date = as.Date(Date, format = "%Y-%m-%d"))
 #SST degrees C, 44N
-SST_44 <- read.csv("SST_44.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
+SST_44 <- read.csv("SST_44.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) %>%
+  transform(Date = sub('T.*', '', Date)) %>%
+  transform(Date = as.Date(Date, format = "%Y-%m-%d"))
 #Upwelling Index, m^3/s/100m coastline, 39N
-Upwell_39 <- read.csv("Upwell_39.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
+Upwell_39 <- read.csv("Upwell_39.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) %>%
+  transform(Date = sub('T.*', '', Date)) %>%
+  transform(Date = as.Date(Date, format = "%Y-%m-%d"))
 #Upwelling Index, m^3/s/100m coastline, 45N
-Upwell_45 <- read.csv("Upwell_45.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
+Upwell_45 <- read.csv("Upwell_45.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE)%>%
+  transform(Date = sub('T.*', '', Date)) %>%
+  transform(Date = as.Date(Date, format = "%Y-%m-%d")) 
 #Meridional Wind, m/s 39N
-Wind_39 <- read.csv("Wind_39.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
+Wind_39 <- read.csv("Wind_39.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) %>%
+  transform(Date = sub('T.*', '', Date)) %>%
+  transform(Date = as.Date(Date, format = "%Y-%m-%d"))
 #Meridional Wind, m/s 44N
-Wind_44 <- read.csv("Wind_44.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
+Wind_44 <- read.csv("Wind_44.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) %>%
+  transform(Date = sub('T.*', '', Date)) %>%
+  transform(Date = as.Date(Date, format = "%Y-%m-%d"))
 
-ocean <- MEI %>% merge(NPGO, by = 'Date', all = T) %>%
-  merge(PDO, by = 'Date', all = T) %>% merge(SST_39, by = 'Date', all = T) %>%
-  merge(SST_44, by = 'Date', all = T) %>% merge(Upwell_39, by = 'Date', all = T) %>% 
-  merge(Upwell_45, by = 'Date', all = T) %>% merge(Wind_39, by = 'Date', all = T) %>% 
-  merge(Wind_44, by = 'Date', all = T) %>%
-  transform(Date = as.Date(Date)) %>%
+ocean <- MEI %>% merge(NPGO, by = 'Date') %>%
+  merge(NOI, by = 'Date') %>%
+  merge(PDO, by = 'Date') %>% 
+  merge(SST_39, by = 'Date') %>%
+  merge(SST_44, by = 'Date') %>% merge(Upwell_39, by = 'Date') %>% 
+  merge(Upwell_45, by = 'Date') %>% merge(Wind_39, by = 'Date') %>% 
+  merge(Wind_44, by = 'Date') %>%
+  #transform(Date = as.Date(Date)) %>%
   transform(ENSO_cat = ifelse(MEI < 0, "La Nina", ifelse(MEI > 0, "El Nino", '')))
 ocean$Year.of.Observation <- as.numeric(format(as.Date(ocean$Date, format = "%Y-%M-%D"),"%Y"))
 ocean$Mo <- as.numeric(format(as.Date(ocean$Date, format = "%Y-%m-%D"),"%m"))
-ocean <- ocean %>% filter(Year.of.Observation > 1988) %>%
+ocean <- ocean %>% filter(Year.of.Observation > 2002) %>%
   transform(Month.of.Observation = ifelse(Mo == 1, 'JAN', 
                                           ifelse(Mo == 2, 'FEB',
                                                  ifelse(Mo == 3, 'MAR',
@@ -98,67 +105,19 @@ ocean <- ocean %>% filter(Year.of.Observation > 1988) %>%
                                                                                                          ifelse(Mo == 11, 'NOV', 'DEC')))))))))))) %>% 
   dplyr::select(-Mo)
 
-#write.csv(ocean, file = "./ocean.csv", row.names = F)
+write.csv(ocean, file = "./ocean.csv", row.names = F)
 
-#Lag 1 month, manually moved oceanographic data one month down, same below
-ocean_lag1 <- read.csv("ocean_lag1.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
-ocean_lag1$Date <- as.Date(ocean_lag1$Date, format = "%m/%d/%y")
-ocean_lag1$Year.of.Observation <- as.numeric(format(as.Date(ocean_lag1$Date, format = "%Y-%M-%D"),"%Y"))
-ocean_lag1$Mo <- as.numeric(format(as.Date(ocean_lag1$Date, format = "%Y-%m-%D"),"%m"))
-ocean_lag1 <- ocean_lag1 %>% filter(Year.of.Observation > 1988) %>%
-  transform(Month.of.Observation = ifelse(Mo == 1, 'JAN', 
-                                          ifelse(Mo == 2, 'FEB',
-                                                 ifelse(Mo == 3, 'MAR',
-                                                        ifelse(Mo == 4, 'APR',
-                                                               ifelse(Mo == 5, 'MAY',
-                                                                      ifelse(Mo == 6, 'JUN',
-                                                                             ifelse(Mo == 7, 'JUL',
-                                                                                    ifelse(Mo == 8, 'AUG',
-                                                                                           ifelse(Mo == 9, 'SEP',
-                                                                                                  ifelse(Mo == 10, 'OCT',
-                                                                                                         ifelse(Mo == 11, 'NOV', 'DEC')))))))))))) %>% 
-  dplyr::select(-Mo)
+#manually adjust date column for lags
+ocean_full <- read.csv("ocean.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) %>%
+  filter(Year.of.Observation > 2002) 
 
-ocean_lag2 <- read.csv("ocean_lag2.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
-ocean_lag2$Date <- as.Date(ocean_lag2$Date, format = "%m/%d/%y")
-ocean_lag2$Year.of.Observation <- as.numeric(format(as.Date(ocean_lag2$Date, format = "%Y-%M-%D"),"%Y"))
-ocean_lag2$Mo <- as.numeric(format(as.Date(ocean_lag2$Date, format = "%Y-%m-%D"),"%m"))
-ocean_lag2 <- ocean_lag2 %>% filter(Year.of.Observation > 1988) %>%
-  transform(Month.of.Observation = ifelse(Mo == 1, 'JAN', 
-                                          ifelse(Mo == 2, 'FEB',
-                                                 ifelse(Mo == 3, 'MAR',
-                                                        ifelse(Mo == 4, 'APR',
-                                                               ifelse(Mo == 5, 'MAY',
-                                                                      ifelse(Mo == 6, 'JUN',
-                                                                             ifelse(Mo == 7, 'JUL',
-                                                                                    ifelse(Mo == 8, 'AUG',
-                                                                                           ifelse(Mo == 9, 'SEP',
-                                                                                                  ifelse(Mo == 10, 'OCT',
-                                                                                                         ifelse(Mo == 11, 'NOV', 'DEC')))))))))))) %>% 
-  dplyr::select(-Mo)
+# ocean_full$Date_lag1 <- as.numeric(format(as.Date(ocean_full$Date_lag1, format = "%m/%d/%y")))
+# ocean_full$Date_lag3 <- as.numeric(format(as.Date(ocean_full$Date_lag3, format = "%m/%d/%y")))
+# ocean_full$Date_lag6 <- as.numeric(format(as.Date(ocean_full$Date_lag6, format = "%m/%d/%y")))
+# # ocean_full$Mo_lag1 <- as.numeric(format(as.Date(ocean_full$Date_lag1, format = "%Y-%m-%D"),"%m"))
+# # ocean_full$Mo_lag3 <- as.numeric(format(as.Date(ocean_full$Date_lag3, format = "%Y-%m-%D"),"%m"))
+# # ocean_full$Mo_lag6 <- as.numeric(format(as.Date(ocean_full$Date_lag6, format = "%Y-%m-%D"),"%m"))
 
-ocean_lag3 <- read.csv("ocean_lag3.csv", header = TRUE, na.strings = "NA", stringsAsFactors = FALSE) 
-ocean_lag3$Date <- as.Date(ocean_lag3$Date, format = "%m/%d/%y")
-ocean_lag3$Year.of.Observation <- as.numeric(format(as.Date(ocean_lag3$Date, format = "%Y-%M-%D"),"%Y"))
-ocean_lag3$Mo <- as.numeric(format(as.Date(ocean_lag3$Date, format = "%Y-%m-%D"),"%m"))
-ocean_lag3 <- ocean_lag3 %>% filter(Year.of.Observation > 1988) %>%
-  transform(Month.of.Observation = ifelse(Mo == 1, 'JAN', 
-                                          ifelse(Mo == 2, 'FEB',
-                                                 ifelse(Mo == 3, 'MAR',
-                                                        ifelse(Mo == 4, 'APR',
-                                                               ifelse(Mo == 5, 'MAY',
-                                                                      ifelse(Mo == 6, 'JUN',
-                                                                             ifelse(Mo == 7, 'JUL',
-                                                                                    ifelse(Mo == 8, 'AUG',
-                                                                                           ifelse(Mo == 9, 'SEP',
-                                                                                                  ifelse(Mo == 10, 'OCT',
-                                                                                                         ifelse(Mo == 11, 'NOV', 'DEC')))))))))))) %>% 
-  dplyr::select(-Mo)
-
-ocean_full <- ocean %>%
-  merge(ocean_lag1, by = c('Year.of.Observation', 'Month.of.Observation'), all = T, suffixes = c("", "_lag1")) %>%
-  merge(ocean_lag2, by = c('Year.of.Observation', 'Month.of.Observation'), all = T, suffixes = c("", "_lag2")) %>%
-  merge(ocean_lag3, by = c('Year.of.Observation', 'Month.of.Observation'), all = T, suffixes = c("", "_lag3"))
 
 #Derive mean SST values
 meanSST <- ocean %>%
@@ -178,16 +137,16 @@ ocean_full <- ocean_full %>%
 # write.csv(anom_lags, "anom_lags.csv", row.names = F)
 
 #Manually added CBA in here, since it is the only "fish" variable that is monthly - may use the other "fish" later
-temp_anom_CBA_lags <- read.csv("temp_anom_CBA_lags.csv", header = T, na.strings = "NA", stringsAsFactors = F) %>%
-  transform(Date = as.Date(Date, format = "%m/%d/%Y"))
+# temp_anom_CBA_lags <- read.csv("temp_anom_CBA_lags.csv", header = T, na.strings = "NA", stringsAsFactors = F) %>%
+#   transform(Date = as.Date(Date, format = "%m/%d/%Y"))
 
-ocean_full <- ocean_full %>%
-  merge(temp_anom_CBA_lags, by = 'Date') %>%
-  transform(Month.of.Observation = Month.of.Observation.y,
-            Year.of.Observation = Year.of.Observation.y) %>%
-  dplyr::select(-c(Month.of.Observation.x, Year.of.Observation.x, Month.of.Observation.y, Year.of.Observation.y)) 
+# ocean_full <- ocean_full %>%
+#   #merge(temp_anom_CBA_lags, by = 'Date') %>%
+#   transform(Month.of.Observation = Month.of.Observation.y,
+#             Year.of.Observation = Year.of.Observation.y) %>%
+#   dplyr::select(-c(Month.of.Observation.x, Year.of.Observation.x, Month.of.Observation.y, Year.of.Observation.y)) 
  
-setwd("~/Documents/R/Strandings/ENSO_Mapping")
+setwd("~/Documents/Research/Strandings/ENSO_Mapping/Env data")
 
 write.csv(ocean_full, file = "./ocean_full.csv", row.names = F)
 write.csv(fish, file = "./fish.csv", row.names = F)
